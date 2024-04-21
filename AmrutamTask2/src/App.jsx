15a4a07767f658e03a8e2c5aad8aae5e85492b8c
vipul <rajvipulraj401@gmail.com>
2024-04-21 12:12:22 +0530

    Made destop 25 in project

--- AmrutamTask2/src/App.jsx
+++ AmrutamTask2/src/App.jsx
@@ -3,29 +3,58 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div>
+    <main>
       {/* First lets divide all of the page
     in one and then we will break the components */}
       {/* WE will start */}
+      <div className="Desktop-24">
+        <div className="header">
+          <div className="headerContent">
+            <h5> Namaste ,Welcome to Amrutam</h5>
 
-      <div className="Header">
-        <div>
-          <h5> Namaste ,Welcome to Amrutam</h5>
+            <h2>
+              Step into Holistic Healing with Ayurveda {/*<br></br>*/} Book
+              Consultation With Certified Experts.
+            </h2>
+            <p>
+              Dive into the world of ayurveda and Experience Personalized Health
+              Solutions and {/*<br></br>*/} Holistic Guidance from Trusted
+              Ayurvedic Doctors Anytime, anywhere.
+            </p>
 
-          <h2>
-            Step into Holistic Healing with Ayurveda <br></br> Book Consultation
-            With Certified Experts.
-          </h2>
-          <p>
-            Dive into the world of ayurveda and Experience Personalized Health
-            Solutions and <br></br>Holistic Guidance from Trusted Ayurvedic
-            Doctors Anytime, anywhere.
-          </p>
-
-          <button>BOOK AN APPOINTMENT</button>
+            <button>BOOK AN APPOINTMENT</button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="Desktop-25">
+        <div className="row1">
+          <h3>Discover Ayurveda's magic with us</h3>
+          <p>
+            Ayurvedic treatment aims to balance your body and mind, bringing
+            harmony and vitality. It's like a journey to better health using
+            ancient wisdom, a totally effective approach for a better life.
+          </p>
+        </div>
+        <div className="row2">
+          <div className="column1">
+            <div className="wellness"></div>
+
+            <div className="prevention"></div>
+            <div className="connection"></div>
+          </div>
+
+          <div className="column2 meditationImg"></div>
+
+          <div className="column3">
+            <div className="wellness"></div>
+
+            <div className="prevention"></div>
+            <div className="connection"></div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
