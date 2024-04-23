de03f51a0c35b73f5a17de42a236888026f61302
vipul <rajvipulraj401@gmail.com>
2024-04-23 17:01:22 +0530

    60% of the project done

--- AmrutamTask2/src/components/Desktop23.jsx
+++ AmrutamTask2/src/components/Desktop23.jsx
@@ -8,8 +8,36 @@ const Desktop23 = () => {
       </div>
       <div className="row2">
         <div className="Experts 1">
-          <div></div>
+          <div className="ExpertContents">
+            <div className="ExpertPic">
+              <img src="ExpertPic.png" alt="" />
+            </div>
+
+            <div className="ExpertRating">4.5 ⭐</div>
+
+            {/* -----------------initials */}
+
+            <div className="ExpertInitials">
+              <h2>Dr. Vaishali Sharma</h2>
+
+              <h3>Ayurveda Practitioner (BAMS ,MD)</h3>
+
+              <div className="Experience">
+                <img src="Hat.png" alt="" />
+                <h4>25 years of experience</h4>
+              </div>
+
+              <div className="initials">
+                <img src="Frame.png" alt="" />
+                <h4>Skin Specialist</h4>
+              </div>
+            </div>
+          </div>
+
+          <div className="greenBox">Book a session</div>
         </div>
+
+        {/* --------------------------------------------- */}
         <div className="Experts  2">
           <div></div>
         </div>
